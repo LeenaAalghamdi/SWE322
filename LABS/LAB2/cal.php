1a4d38fac7cfea5d083caa98f8edc04817f58fdb
LeenaAalghamdi <144690996+LeenaAalghamdi@users.noreply.github.com>
2023-09-11 15:46:20 +0300

    Create cal.php

---
+++ LABS/LAB2/cal.php
@@ -0,0 +1,19 @@
+<?php
+
+function add ($x,$y){
+	return ($x+$y);
+}
+function subtarct ($x,$y){
+	return ($x-$y);
+}
+function multiply ($x,$y){
+	return ($x*$y);
+}
+function devide($x,$y){
+	return ($x/$y);
+}
+echo "5+10=".add(5,10)."<br>";
+echo "13-7=".subtract(13,7)."<br>";
+echo "5*4=".multiply(5,4)."<br>";
+echo"3/5=".devide(3,5)."<br>";
+?> 
